99bfdbdce1ba9c57bbc4a1f79999cfeb350e797d
framadhan99 <fajarramadhan99704@gmail.com>
2023-03-17 10:06:32 +0700

    create config

---
+++ lib/config/asset_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AssetColor {
+  static const Color blueLight = Color(0xffE3F2FC);
+  static const Color blue = Color(0xff3282B8);
+  static const Color blueDrak = Color(0xff0F4C75);
+  static const Color blueDraked = Color(0xff1B262C);
+}
---
+++ lib/config/asset_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:presence_app/config/asset_colors.dart';
+
+class AssetStyle {
+  static const TextStyle textTitle = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AssetColor.blueDraked,
+  );
+
+  static const TextStyle textPrimary = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AssetColor.blueDraked,
+  );
+  static const TextStyle textLabel = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+}
